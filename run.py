c90ff7395a4ef91e2ba59df4bc93c9e1cb016721
ssz7.ch2 <ssz7.ch2@gmail.com>
2024-11-30 18:57:41 -0500

    add stable score option

--- run.py
+++ run.py
@@ -5,11 +5,12 @@ import argparse
 import sys
 from typing import Iterable
 from dotenv import load_dotenv
-from ossapi import BeatmapsetSearchCategory, Ossapi, BeatmapsetSearchSort
+from ossapi import BeatmapsetSearchCategory, Ossapi, BeatmapsetSearchSort, Score
 from time import sleep
-from circleguard import Circleguard, Judgment, JudgmentType, ReplayUnavailableException
+from circleguard import Circleguard, Judgment, JudgmentType, ReplayCache, ReplayUnavailableException
 from slider import Beatmap, Library, Slider, Spinner
 from copy import deepcopy
+from ossapi.models import NonLegacyMod
 
 load_dotenv()
 
@@ -87,10 +88,12 @@ def get_ranked_beatmap_ids(path):
   
   return ranked_beatmap_ids
 
-def _run(replay, beatmap, beatmap_id, logging=False, save=True):
+def _run(replay, beatmap, beatmap_id, logging=False, save=True, combo_progress=None, stable_score=None):
   object_c = object_count(deepcopy(beatmap))
 
   judgments, classic_combo_progress = cg.judgments(replay, beatmap=deepcopy(beatmap), slider_acc=False)
+  if combo_progress is not None:
+    classic_combo_progress = combo_progress
   calculated = judgment_counts(judgments)
 
   # circleguard judgments doesn't include spinners, so we assume all spinners are 300s
@@ -113,23 +116,29 @@ def _run(replay, beatmap, beatmap_id, logging=False, save=True):
   # number of sliderends hit
   count_sliderends = object_c["sliderends"] - missed_sliderends
 
-  judgments, combo_progress = cg.judgments(replay, beatmap=deepcopy(beatmap), slider_acc=True)
+  judgments, lazer_combo_progress = cg.judgments(replay, beatmap=deepcopy(beatmap), slider_acc=True)
+  if combo_progress is not None:
+    lazer_combo_progress = combo_progress
   calculated = judgment_counts(judgments)
   calculated["count_300"] += object_c["spinners"]
   if logging:
     print("lazer calculated", calculated)
-    print("lazer combo progress", combo_progress)
+    print("lazer combo progress", lazer_combo_progress)
 
   # slider heads in stable also count as a slidertick
   # sliderend count can be used here since # of sliderends == # of sliderheads
   total_sliderticks_classic = object_c["sliderticks"] + object_c["sliderends"]
   classic_total_score = compute_total_score(replay.count_300, replay.count_100, replay.count_50, replay.count_miss, count_sliderends, object_c["sliderends"], total_sliderticks_classic, classic=True, combo_progress=classic_combo_progress)
-  total_score = compute_total_score(calculated["count_300"], calculated["count_100"], calculated["count_50"], calculated["count_miss"], count_sliderends, object_c["sliderends"], object_c["sliderticks"], combo_progress=combo_progress)
+  total_score = compute_total_score(calculated["count_300"], calculated["count_100"], calculated["count_50"], calculated["count_miss"], count_sliderends, object_c["sliderends"], object_c["sliderticks"], combo_progress=lazer_combo_progress)
 
   if logging:
     print(f"\nlazer score:\t{total_score}")
     print(f"classic score:\t{classic_total_score}")
-    print(f"ratio:\t\t{total_score / classic_total_score}")
+    if stable_score is not None:
+      print(f"stable score:\t{stable_score}")
+      print(f"ratio:\t\t{total_score / stable_score}")
+    else:
+      print(f"ratio:\t\t{total_score / classic_total_score}")
 
   # skip replays with incorrect misses due to issues with circleguard
   if missed_sliderends < 0 or sliderbreaks < 0:
@@ -140,25 +149,69 @@ def _run(replay, beatmap, beatmap_id, logging=False, save=True):
 
   if save:
     with open("data.txt", "a") as f:
-      f.write(f"https://osu.ppy.sh/beatmaps/{beatmap_id}\t{replay.user_id}\t{classic_total_score}\t{total_score}\n")
+      if stable_score is not None:
+        f.write(f"https://osu.ppy.sh/beatmaps/{beatmap_id}\t{replay.user_id}\t{stable_score}\t{classic_total_score}\t{total_score}\n")
+      else:
+        f.write(f"https://osu.ppy.sh/beatmaps/{beatmap_id}\t{replay.user_id}\t{classic_total_score}\t{total_score}\n")
 
-def run(beatmap_ids=None, amount=1, start = 1, end=50, sample_size=10, logging=False, classic_lb=True, save=True, path="beatmap_ids.json"):
+def get_score_multiplier(mods: list[NonLegacyMod]):
+  multiplier = 1.0
+  for mod in mods:
+    if mod.acronym == "CL":
+      multiplier *= 0.96
+    if mod.acronym == "HD":
+      multiplier *= 1.06
+    if mod.acronym == "HR":
+      multiplier *= 1.06
+    if mod.acronym == "DT":
+      multiplier *= 1.10
+    if mod.acronym == "FL":
+      multiplier *= 1.12
+    if mod.acronym == "HT":
+      multiplier *= 0.3
+    if mod.acronym == "NF":
+      multiplier *= 0.5
+    if mod.acronym == "EZ":
+      multiplier *= 0.5
+    if mod.acronym == "SO":
+      multiplier *= 0.9
+  return multiplier
+
+def get_combo_progress_from_score(score: Score):
+  return (score.total_score_without_mods - 500000 * pow(score.accuracy, 5)) / (500000 * score.accuracy)
+
+def run(beatmap_ids=None, amount=1, start = 1, end=50, sample_size=10, logging=False, classic_lb=True, save=True, path="beatmap_ids.json", stable=False):
   if beatmap_ids is None:
     ranked_beatmap_ids = get_ranked_beatmap_ids(path)
     beatmap_ids = random.sample(ranked_beatmap_ids, min(amount, len(ranked_beatmap_ids)))
 
   for beatmap_id in beatmap_ids:
-    user_ids = [score.user_id for score in api.beatmap_scores(beatmap_id, mode="osu", legacy_only=classic_lb, limit=100).scores if score.score > 0][max(start - 1, 0):end]
-    user_ids = random.sample(user_ids, min(sample_size, len(user_ids)))
+    beatmap_scores = [(score.user_id, score) for score in api.beatmap_scores(beatmap_id, mode="osu", legacy_only=classic_lb, limit=100).scores if score.legacy_score_id is not None][max(start - 1, 0):end]
+    beatmap_scores = random.sample(beatmap_scores, min(sample_size, len(beatmap_scores)))
 
     beatmap = None
-    for user_id in user_ids:
+    for (user_id, score) in beatmap_scores:
       try:
-        replay = cg.ReplayMap(beatmap_id, user_id)
+        try:
+          replay = ReplayCache("circleguard.db", beatmap_id, user_id)
+          cg.load(replay)
+          replay = replay.replay
+
+          replay.count_300 = score.statistics.great if score.statistics.great is not None else 0
+          replay.count_100 = score.statistics.ok if score.statistics.ok is not None else 0
+          replay.count_50 = score.statistics.meh if score.statistics.meh is not None else 0
+          replay.count_miss = score.statistics.miss if score.statistics.miss is not None else 0
+        except:
+          replay = cg.ReplayMap(beatmap_id, user_id)
         if beatmap is None:
           beatmap = replay.beatmap(library)
 
-        _run(replay, beatmap, beatmap_id, logging=logging, save=save)
+        combo_progress = None
+        if stable:
+          score.total_score_without_mods = int(round(score.total_score / get_score_multiplier(score.mods)))
+          combo_progress = get_combo_progress_from_score(score)
+
+        _run(replay, beatmap, beatmap_id, logging=logging, save=save, combo_progress=combo_progress, stable_score=score.total_score_without_mods)
 
         sleep(6)
       except ReplayUnavailableException:
@@ -169,11 +222,19 @@ def run(beatmap_ids=None, amount=1, start = 1, end=50, sample_size=10, logging=F
       except:
         print("unable to process replay.", "beatmap:", beatmap_id, "user_id:", user_id)
 
-def run_user(beatmap_id, user_id, logging=False, save=False):
+def run_user(beatmap_id, user_id, logging=False, save=False, stable=False):
   replay = cg.ReplayMap(beatmap_id, user_id)
   beatmap = replay.beatmap(library)
 
-  _run(replay, beatmap, beatmap_id, logging=logging, save=save)
+  score = api.beatmap_user_score(beatmap_id, user_id, mode="osu", mods=replay.mods).score
+
+  combo_progress = None
+  score.total_score_without_mods = None
+  if stable:
+    score.total_score_without_mods = int(round(score.total_score / get_score_multiplier(score.mods)))
+    combo_progress = get_combo_progress_from_score(score)
+
+  _run(replay, beatmap, beatmap_id, logging=logging, save=save, combo_progress=combo_progress, stable_score=score.total_score_without_mods)
 
 def run_folder(path, logging=False, save=False):
   replay_paths = [filename for filename in os.listdir(path) if filename.endswith("osr")]
@@ -207,7 +268,8 @@ def main():
   parser.add_argument('--start', type=int, default=1, help="The leaderboard spot to start from")
   parser.add_argument('--end', type=int, default=10, help="The leaderboard spot to end on")
   parser.add_argument('--sample-size', type=int, default=10, help="Amount of replays to use for each beatmap")
-  parser.add_argument('--lazer', action=argparse.BooleanOptionalAction, default=False, help="Use lazer leaderboard")
+  parser.add_argument('--lazer-lb', action=argparse.BooleanOptionalAction, default=False, help="Use lazer leaderboard sorting")
+  parser.add_argument('--stable', action=argparse.BooleanOptionalAction, default=False, help="Use stable's converted score")
 
   args = parser.parse_args()
 
@@ -216,14 +278,14 @@ def main():
     return
   
   if args.user_id is not None:
-    run_user(args.beatmap_ids[0], args.user_id, logging=args.logging, save=args.save)
+    run_user(args.beatmap_ids[0], args.user_id, logging=args.logging, save=args.save, stable=args.stable)
     return
   
   if args.folder is not None:
     run_folder(args.folder, logging=args.logging, save=args.save)
     return
   
-  run(beatmap_ids=args.beatmap_ids, amount=args.count, start=args.start, end=args.end, sample_size=args.sample_size, logging=args.logging, classic_lb=not args.lazer, save=args.save)
+  run(beatmap_ids=args.beatmap_ids, amount=args.count, start=args.start, end=args.end, sample_size=args.sample_size, logging=args.logging, classic_lb=not args.lazer, save=args.save, stable=args.stable)
 
 if __name__ == "__main__":
   main()
